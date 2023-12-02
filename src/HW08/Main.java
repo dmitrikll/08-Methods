9e70cda0839c7dd8873d0350abea3c9b5ee31f4e
Dmytro Halytskyi <ds.galytskyi@gmail.com>
2023-12-02 18:58:53 +0200

    add start files

---
+++ src/HW08/Main.java
@@ -0,0 +1,31 @@
+package HW08;
+
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class Main {
+
+    static double salesAmount;
+    static double purhaseAmount;
+    static double discountAmount;
+    final static double STANDARDDISCOUNT = 5;
+    final static double AVERAGEDISCOUNT = 10;
+    final static double HIGHDISCOUNT = 15;
+    final static double MINPURSHASEAMOUNT = 5000;
+    final static double MAXPURSHASEAMOUNT = 10000;
+
+    public static void main(String[] args) {
+        // TODO виклик методів
+    }
+
+}
+
+        /*
+        1. сканер ввод суми покупки salesAmount
+        2. if - else = MIN MAX = discount
+        3. discount standard
+        4. discount average
+        5. discount max
+        6. print result
+        7. main
+         */
