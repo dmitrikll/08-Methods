5091bd19f4a15cb0c6f8fcb4690254e0f96f3d71
Dmytro Halytskyi <ds.galytskyi@gmail.com>
2023-12-03 23:08:31 +0200

    fix constants name

--- src/HW08/Main.java
+++ src/HW08/Main.java
@@ -6,23 +6,23 @@ import java.text.DecimalFormat;
 
 public class Main {
 
-    static double purshaseAmount;
+    static double purchaseAmount;
     static double discount;
     static double amountPay;
     static double amountDiscount;
-    final static double STANDARDDISCOUNT = 5;
-    final static double AVERAGEDISCOUNT = 10;
-    final static double HIGHDISCOUNT = 15;
-    final static double MINPURSHASEAMOUNT = 5000;
-    final static double MAXPURSHASEAMOUNT = 10000;
+    final static double STANDARD_DISCOUNT = 5;
+    final static double AVERAGE_DISCOUNT = 10;
+    final static double HIGH_DISCOUNT = 15;
+    final static double MIN_PURCHASE_AMOUNT = 5000;
+    final static double MAX_PURCHASE_AMOUNT = 10000;
     final static String CURRENCY = "USD";
 
     public static void main(String[] args) {
 
-        purshaseAmount = getData();
-        discount = getDiscount(purshaseAmount);
-        amountPay = calculatePay(purshaseAmount, discount);
-        amountDiscount = calculateDiscount(purshaseAmount, discount);
+        purchaseAmount = getData();
+        discount = getDiscount(purchaseAmount);
+        amountPay = calculatePay(purchaseAmount, discount);
+        amountDiscount = calculateDiscount(purchaseAmount, discount);
         getOutput();
     }
 
@@ -36,12 +36,12 @@ public class Main {
 
     public static double getDiscount(double amount) {
 
-        if (amount <= MINPURSHASEAMOUNT) {
-            return STANDARDDISCOUNT;
-        } else if (amount > MINPURSHASEAMOUNT && purshaseAmount <= MAXPURSHASEAMOUNT) {
-            return AVERAGEDISCOUNT;
+        if (amount <= MIN_PURCHASE_AMOUNT) {
+            return STANDARD_DISCOUNT;
+        } else if (amount > MIN_PURCHASE_AMOUNT && purchaseAmount <= MAX_PURCHASE_AMOUNT) {
+            return AVERAGE_DISCOUNT;
         } else {
-            return HIGHDISCOUNT;
+            return HIGH_DISCOUNT;
         }
     }
 
@@ -57,12 +57,12 @@ public class Main {
 
     public static void getOutput() {
 
-        String roundPurshaseAmount = roundValue(purshaseAmount);
+        String roundPurchaseAmount = roundValue(purchaseAmount);
         String roundDiscount = roundValue(discount);
         String roundAmountDiscount = roundValue(amountDiscount);
         String roundAmountPay = roundValue(amountPay);
 
-        System.out.println("Total purchase amount: " + CURRENCY + " " + roundPurshaseAmount +
+        System.out.println("Total purchase amount: " + CURRENCY + " " + roundPurchaseAmount +
                 "\nYour discount: " + roundDiscount + " %" +
                 "\nAmount of discount: " + CURRENCY + " " + roundAmountDiscount +
                 "\nThe amount to be paid: " + CURRENCY + " " + roundAmountPay);
